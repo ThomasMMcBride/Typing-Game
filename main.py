5c2859baf14286a4a6719a4455c67dfc1df19ed1
Thomas M. McBride <thomasmcbridebusiness@gmail.com>
2025-01-01 13:32:48 -0500

    Initial

---
+++ main.py
@@ -0,0 +1,9 @@
+from controller import TypingGameController
+
+def main():
+    #TODO: Create a controller and call run.
+    controller = TypingGameController()
+    controller.run()
+
+if __name__ == "__main__":
+    main()
